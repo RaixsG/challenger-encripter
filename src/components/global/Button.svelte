8f91c4f55f85922d153255d8d6ba69bd008a0012
RaixsG <jhardelloficial@gmail.com>
2024-07-03 22:34:34 -0500

    first commit

---
+++ src/components/global/Button.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let title;
+
+    let classButton;
+    $: classButton = ' p-4 w-8 '
+</script>
+
+<button class={classButton} on:click={() => (console.log('boton'))}>
+    {title}
+</button>
+
